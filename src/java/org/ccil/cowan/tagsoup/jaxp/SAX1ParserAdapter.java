1b44790acc147e51caed5b81d75141efa7334266
John Cowan <cowan@ccil.org>
2007-03-25 02:40:39 +0000

    Added notices to JAXP classes Updated notices elsewhere from AFL 2.1 to AFL 3.0

--- src/java/org/ccil/cowan/tagsoup/jaxp/SAX1ParserAdapter.java
+++ src/java/org/ccil/cowan/tagsoup/jaxp/SAX1ParserAdapter.java
@@ -1,3 +1,15 @@
+// This file is part of TagSoup.
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.  You may also distribute
+// and/or modify it under version 3.0 of the Academic Free License.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
 package org.ccil.cowan.tagsoup.jaxp;
 
 import java.io.IOException;
